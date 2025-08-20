d1724331056849d32136c7315ddb3cb6bf62361c
Santiago Figueredo <figueredosantiago2000@gmail.com>
2025-08-19 22:33:56 -0300

    Nueva carpeta 'Lote de carga y proceso'

---
+++ Lote de Carga y Proceso/Ejercicio-1/ejercicio-1/Program.cs
@@ -0,0 +1,109 @@
+﻿namespace ejercicio_1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        /*2. Se dispone de un lote de 300 registros (uno por artículo), cada uno de 
+        los cuales tiene la
+        siguiente información:
+        - Número de artículo (4 dígitos no correlativos).
+        - Cantidad de unidades en stock.
+        - Precio Unitario.
+        El lote está desordenado.
+        Se dispone de otro lote de registros, cada uno de los cuales corresponde a una venta con la siguiente
+        información:
+
+        - Número de cliente (1 a 300).
+        - Número de artículo (4 dígitos, no correlativos).
+        - Cantidad de unidades vendidas.
+
+        El último registro de este lote tiene número de cliente cero y no debe procesarse. Desarrollar el
+        programa que determine e imprima:
+        a) Un listado de las ventas realizadas, con el siguiente formato:
+
+        Listado de ventas
+
+        Nro. Cliente Nro. Artículo Cantidad Vendida Importe Total
+        99 99 999 999.99
+        b) Informar cuál es el número de cliente que más compró en total (en pesos).
+        c) Informar cada uno de los números de los artículos que no hayan registrado ventas.*/
+
+        int[] numArticulos = new int[300];
+        int[] cantidadUnidadesVendidias = new int[300];
+        int[] precioUnitario = new int[300];
+
+
+        for (int x = 0; x < 300; x++) {
+
+            Console.WriteLine("Ingrese numero de articulo");
+            numArticulos[x] = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese cantidad vendida");
+            cantidadUnidadesVendidias[x] = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese precio unitario");
+            precioUnitario[x] = int.Parse(Console.ReadLine());
+    
+        }
+
+        Console.WriteLine("Ingrese numero de cliente");
+        int nDeCliente = int.Parse(Console.ReadLine());
+        Console.WriteLine("Ingrese numero de cliente");
+        int numArticulo = int.Parse(Console.ReadLine());
+        Console.WriteLine("Ingrese numero de cliente");
+        int cantUniVen = int.Parse(Console.ReadLine());
+
+        int[] numeroDeCliente = new int[300];
+        int[] cantUniVendidas = new int[300];
+        int[] numArt = new int[300];
+        int[] importeTotal = new int[300];
+
+        int indice = 0;
+
+        while(nDeCliente != 0)
+        {
+
+            for (int x = 0; x < 300; x++)
+            {
+                if(numArticulos[x] == numArt[x])
+                {
+                    importeTotal[indice] = precioUnitario[x] * cantUniVendidas[x];
+                }
+            }
+
+            numeroDeCliente[indice] = nDeCliente;
+            numArt[indice] = numArticulo;
+            cantUniVendidas[indice] = cantUniVen;
+
+            indice++;
+
+            Console.WriteLine("Ingrese numero de cliente");
+            nDeCliente = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese numero de articulo");
+            numArticulo = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese cantidad de unidades vendidas");
+            cantUniVen = int.Parse(Console.ReadLine());
+
+        }
+
+        int max = importeTotal[0];
+        int clienteMasCompro = 0;
+
+        for (int x = 0; x < 300; x++)
+        {
+            if (importeTotal[x] > max)
+            {
+                clienteMasCompro = numeroDeCliente[x];
+            }
+        }
+
+        Console.WriteLine("Cliente que mas compro " + clienteMasCompro);
+
+        for (int x = 0; x < 300; x++)
+        {
+            if (numArticulos[x] == 0)
+            {
+                Console.WriteLine("El articulo " + numArticulos[x] + " no tuvo ventas");
+            }
+        }
+    }
+}
